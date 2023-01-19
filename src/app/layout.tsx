2f972b6965ca932170a692c384c4b9a3fa8fa416
bightg30098 <bightg30098@gmail.com>
2023-01-19 21:55:17 +0800

    chore: update folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import Header from '@/components/header/Header'
 import '@/styles/globals.css'
+
+import Header from '@/components/Header'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { useMemo } from 'react'
+
+import clsx from 'clsx'
+
+import NavLink from '@/headless/NavLink'
+import { routes } from '@/router/routes'
+
+export default function Navbar() {
+  const _routes = useMemo(() => routes(), [])
+  return (
+    <nav>
+      <ul className="flex space-x-4">
+        {_routes.map(({ path, pathAlias, label }) => (
+          <NavLink
+            href={path}
+            hrefAlias={pathAlias}
+            key={path}
+            className={({ isActive }) => clsx(isActive ? 'text-gray-50' : 'text-gray-200 hover:text-gray-100')}
+          >
+            {({ isActive }) => (
+              <>
+                <span
+                  className={clsx(
+                    'border-b-2 px-1 pb-1 text-lg font-medium text-current transition-colors duration-200',
+                    isActive ? ' border-b-primary-600' : 'border-b-transparent',
+                  )}
+                >
+                  {label}
+                </span>
+              </>
+            )}
+          </NavLink>
+        ))}
+      </ul>
+    </nav>
+  )
+}
