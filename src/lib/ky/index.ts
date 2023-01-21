2c703142bcecd26906e421383c5c4b9bb61d7cab
bightg30098 <bightg30098@gmail.com>
2023-01-22 01:20:58 +0800

    feat: add ky after response hook

--- src/lib/ky/index.ts
+++ src/lib/ky/index.ts
@@ -2,4 +2,21 @@ import ky from 'ky'
 
 export default ky.create({
   prefixUrl: process.env.BACKEND_API_URL,
+  throwHttpErrors: true,
+  hooks: {
+    ...(process.env.NODE_ENV === 'development' && {
+      afterResponse: [
+        (_request, _options, response) => {
+          // clone response to avoid consuming it
+          const cloneResponse = response.clone()
+          cloneResponse.json().then((json) => {
+            console.log([response.status], response.url)
+            console.log(JSON.stringify(json))
+            console.log('----------------------------------------\n')
+          })
+          return response
+        },
+      ],
+    }),
+  },
 })
